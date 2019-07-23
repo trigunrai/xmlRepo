38045d23e5dc9f352ceb3677d618d8956cb884c1
trigunrai <trigunrai15@gmail.com>
2019-07-22 21:01:02 -0700

    Assignment 1

---
+++ XmlAssignmentCaseStudy1/src/com/zensar/xml/sax/parsers/OrderXmlHandler.java
@@ -0,0 +1,42 @@
+package com.zensar.xml.sax.parsers;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class OrderXmlHandler extends DefaultHandler {
+
+	@Override
+	public void characters(char[] arg0, int arg1, int arg2) throws SAXException {
+		// TODO Auto-generated method stub
+		for (int i=arg1;i<arg1+arg2;i++){
+			System.out.print(arg0[i]);
+		}
+		System.out.println();
+	}
+
+	@Override
+	public void endDocument() throws SAXException {
+		// TODO Auto-generated method stub
+		System.out.println("End of XML Document");
+	}
+
+	@Override
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		// TODO Auto-generated method stub
+		System.out.println("End Element:"+qName);
+	}
+
+	@Override
+	public void startDocument() throws SAXException {
+		// TODO Auto-generated method stub
+		System.out.println("Starting...");
+	}
+
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+		// TODO Auto-generated method stub
+		System.out.println("Start Element:"+qName);
+	}
+
+}
